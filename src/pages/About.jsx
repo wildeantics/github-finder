f1b38104803ac327eba3f4fae5868b6eb5c4ec04
Morgan Wilde <morgan@wilde.mx>
2022-05-10 16:08:46 +1000

    init commit v1

---
+++ src/pages/About.jsx
@@ -0,0 +1,22 @@
+function About() {
+  return (
+    <>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details. Morgan
+        made this project as part of the
+        <a href='https://www.udemy.com/course/react-front-to-back-2022/'>
+          {" "}
+          React Front To Back 2022
+        </a>{" "}
+        Udemy course by
+        <strong>
+          <a href='https://traversymedia.com'> Brad Traversy</a>
+        </strong>
+        .
+      </p>
+    </>
+  );
+}
+
+export default About;
