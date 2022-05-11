d8f1ea6929d2072829a030de2fd11d3af212b0b5
Morgan Wilde <morgan@wilde.mx>
2022-05-11 15:38:46 +1000

    Moved title onto page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
-import UserResults from "../components/users/UserResults";
-import UserSearch from "../components/users/UserSearch";
+import UserResults from '../components/users/UserResults'
+import UserSearch from '../components/users/UserSearch'
+import { FaGithub } from 'react-icons/fa'
+
 function Home() {
   return (
     <>
+      <h1 className='text-6xl card-title mb-6 text-white'>
+        <FaGithub />
+        Github Finder
+      </h1>
       <UserSearch />
       <UserResults />
     </>
-  );
+  )
 }
 
-export default Home;
+export default Home
